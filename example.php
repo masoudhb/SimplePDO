11f847ff5c6e50e2c84b7eb01035b622ea0a87a4
Masoud HassanBeygi <masoudhb@live.com>
2016-11-15 12:20:20 +0330

    Update example.php

--- example.php
+++ example.php
@@ -4,16 +4,16 @@ require_once 'SimplePDO.php';
 
 $pdo = new SimplePDO('dbtest', 'root', '');
 
-$rows = $pdo->rows('tbltest', array(array('id', '>', 0), "AND", array('user_id', '=', 2)));
+$rows = $pdo->rows('tbltest', array(array('id', '>', 0), "AND", array('title', '=', 'abc')));
 var_dump($rows);
 
-$select = $pdo->select('tbltest', '*', false, array('id', 'desc'));
+$select = $pdo->select('tbltest', '*', array(array('title', '=', 'abc')), array('id', 'desc'));
 var_dump($select);
 
-$insert = $pdo->insert('tbltest', array('user_id' => '2', 'title' => 'test', 'content' => 'test', 'date' => '1479129508'));
+$insert = $pdo->insert('tbltest', array('title' => 'test'));
 var_dump($insert);
 
-$update = $pdo->update('tbltest', array('user_id' => '3'), array(array('id', '=', '2')));
+$update = $pdo->update('tbltest', array('title' => 'masoud'), array(array('id', '=', '5')));
 var_dump($update);
 
 $delete = $pdo->delete('tbltest', array(array('id', '!=', '1'), 'AND', array('id', '<>', '2')));
